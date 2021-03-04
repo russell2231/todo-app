c0f0be8c1fc6ff1f475b66393504a0afa0a76798
Russell Rice <russellrice15@gmail.com>
2021-03-04 11:36:45 -0500

    Add interactivity to clear completed tasks, and filter list

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ const body = document.querySelector('body');
 const themeToggleBtn = document.querySelector('.theme-toggle');
 const todoForm = document.querySelector('#todoForm');
 const todoList = document.querySelector('.todos');
+const filterList = document.querySelector('.list-filter .list');
+const clearCompletedBtn = document.querySelector('.clear');
 const todoListItems = localStorage.getItem('todoList')
 	? JSON.parse(localStorage.getItem('todoList'))
 	: [];
@@ -38,14 +40,9 @@ function showTodo(todo) {
 	localStorage.setItem('todoList', JSON.stringify(todoListItems));
 
 	const item = document.querySelector(`[data-key='${todo.id}']`);
-
+	const itemsLeft = document.querySelector('.items-left');
 	const isCompleted = todo.isCompleted ? 'done' : '';
 	const li = document.createElement('li');
-
-	if (todo.deleted) {
-		item.remove();
-		return;
-	}
 
 	li.setAttribute('class', `list-item ${isCompleted}`);
 	li.setAttribute('data-key', todo.id);
@@ -60,10 +57,20 @@ function showTodo(todo) {
     <img class="close" src="images/icon-cross.svg" alt="" />
   `;
 
+	itemsLeft.textContent = `${isLeft(todoListItems)} items left`;
+
+	if (todo.deleted) {
+		item.remove();
+		itemsLeft.textContent = `${isLeft(todoListItems)} items left`;
+		return;
+	}
+
 	if (item) {
 		item.replaceWith(li);
+		filterTodo();
 	} else {
 		todoList.append(li);
+		filterTodo();
 	}
 }
 
@@ -74,6 +81,47 @@ function toggleComplete(key) {
 
 	todoListItems[index].isCompleted = !todoListItems[index].isCompleted;
 	showTodo(todoListItems[index]);
+}
+
+// Items Left
+
+function isLeft(list) {
+	let todoLeft = 0;
+	list.forEach((todo) => {
+		if (!todo.isCompleted) {
+			todoLeft++;
+		}
+	});
+	return todoLeft;
+}
+
+// Filter List
+
+function filterTodo() {
+	const todoList = document.querySelectorAll('.todos .list-item');
+	const filterOption = document.querySelector('.list-filter .selected');
+
+	if (filterOption.textContent === 'Active') {
+		todoList.forEach((todo) => {
+			if (todo.classList.contains('done')) {
+				todo.style.display = 'none';
+			} else {
+				todo.style.display = 'flex';
+			}
+		});
+	} else if (filterOption.textContent === 'Completed') {
+		todoList.forEach((todo) => {
+			if (todo.classList.contains('done')) {
+				todo.style.display = 'flex';
+			} else {
+				todo.style.display = 'none';
+			}
+		});
+	} else {
+		todoList.forEach((todo) => {
+			todo.style.display = 'flex';
+		});
+	}
 }
 
 // Switch between light and dark theme
@@ -117,16 +165,36 @@ todoForm.addEventListener('submit', (e) => {
 	}
 });
 
-// Mark complete or delete todo
 todoList.addEventListener('click', (e) => {
+	// Mark Complete
 	if (e.target.classList.contains('check')) {
 		const itemKey =
 			e.target.parentElement.parentElement.parentElement.dataset.key;
 		toggleComplete(itemKey);
 	}
 
+	// Delete Todo
 	if (e.target.classList.contains('close')) {
 		const itemKey = e.target.parentElement.dataset.key;
 		deleteTodo(itemKey);
 	}
+});
+
+// Clear All Completed Tasks
+clearCompletedBtn.addEventListener('click', () => {
+	todoListItems.forEach((todo) => {
+		todo.isCompleted = false;
+		showTodo(todo);
+	});
+});
+
+// Filter Todo List
+filterList.addEventListener('click', (e) => {
+	const filterOptions = document.querySelectorAll('.list-filter li');
+
+	filterOptions.forEach((option) => option.classList.remove('selected'));
+
+	e.target.classList.add('selected');
+
+	filterTodo();
 });
